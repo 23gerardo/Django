e0d24d1b8ab4d884c36ee54cc7f186146f1a3a97
23gerardo <23gerardo@gmail.com>
2018-02-08 09:12:57 -0400

    se agrego los namespase

--- refugio/urls.py
+++ refugio/urls.py
@@ -19,7 +19,7 @@ from django.contrib import admin
 
 urlpatterns = [
     path('admin/', admin.site.urls),
-    url(r'^mascota/', include ('apps.mascota.urls')),
-    url(r'^adopcion/', include ('apps.adopcion.urls')),
+    url(r'^mascota/', include ('apps.mascota.urls',namespace="mascota")),
+    url(r'^adopcion/', include ('apps.adopcion.urls',namespace="adopcion")),
 
 ]
