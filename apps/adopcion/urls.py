1c029dcfef3c5c7ba718069b31434195134dab69
23gerardo <23gerardo@gmail.com>
2018-02-09 10:49:39 -0400

    se corrigio la falla en la carpeta template que no permitia correr las urls y se implemento el archivo base  con el index mascota

--- apps/adopcion/urls.py
+++ apps/adopcion/urls.py
@@ -1,5 +1,5 @@
 from django.conf.urls import url
-from apps.adopcion.views import index_adopcion 
+from apps.adopcion.views import index_adopcion
 
 urlpatterns = [
     url(r'^$',index_adopcion),
--- apps/mascota/urls.py
+++ apps/mascota/urls.py
@@ -1,6 +1,7 @@
 from django.conf.urls import url, include
-from apps.mascota.views import index 
+from apps.mascota.views import index
 
 urlpatterns = [
-    url(r'^$',index),
+   url(r'^$', index),
+
 ]
